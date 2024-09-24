1cc8a8fb2a5592f993b9a8a182acd2b82949851b
Juanderer Virtual tour <juanderervtour@gmail.com>
2024-09-24 08:22:42 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,671 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrance",
+      "name": "Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.16302166462515189,
+        "pitch": -0.3278792533779722,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.01585532421218261,
+          "pitch": 0.17286272518357748,
+          "rotation": 0,
+          "target": "1-grand-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.23284451318069443,
+          "pitch": -0.009184961497908262,
+          "title": "Entrada De Villa Sariaya<div>(Entrance of Villa Sariaya)</div>",
+          "text": "The primary entrance of Villa Sariaya features double wooden doors that reflect the craftsmanship and artistry of the past."
+        }
+      ]
+    },
+    {
+      "id": "1-grand-entrance",
+      "name": "Grand Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.1264473233587715,
+        "pitch": -0.07759364185645445,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9850840616661714,
+          "pitch": 0.19656845879273632,
+          "rotation": 0,
+          "target": "12-kitchen"
+        },
+        {
+          "yaw": -1.5601446507483239,
+          "pitch": 0.321349500301336,
+          "rotation": 0,
+          "target": "2-pasillo-hallway"
+        },
+        {
+          "yaw": 3.1357644073629363,
+          "pitch": 0.38773367180641216,
+          "rotation": 0,
+          "target": "0-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.02797801075342221,
+          "pitch": -0.0011418827470741633,
+          "title": "Gran Entrada&nbsp;<div>(Grand Entrance)</div>",
+          "text": "The current grand entrance of Villa Sariaya, it was commonly used by servants during its period."
+        }
+      ]
+    },
+    {
+      "id": "2-pasillo-hallway",
+      "name": "Pasillo (Hallway)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -0.5239051858589825,
+        "pitch": -0.1001208282018542,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22092902403094605,
+          "pitch": 0.32537850267920376,
+          "rotation": 0,
+          "target": "3-receiving-area"
+        },
+        {
+          "yaw": 1.4670999436734231,
+          "pitch": 0.4597312736168142,
+          "rotation": 0,
+          "target": "1-grand-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-receiving-area",
+      "name": "Receiving Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -0.007123792865286305,
+        "pitch": -0.047584744363650344,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.22604407249121294,
+          "pitch": 0.3100651699257959,
+          "rotation": 0,
+          "target": "4-old-grand-entrance"
+        },
+        {
+          "yaw": 3.069801129224368,
+          "pitch": 0.42545759066279665,
+          "rotation": 0,
+          "target": "2-pasillo-hallway"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5791662042217034,
+          "pitch": -0.027228827048311643,
+          "title": "Recibidor (Receiving Area)",
+          "text": "The area that is often used for receiving visitors or guests."
+        }
+      ]
+    },
+    {
+      "id": "4-old-grand-entrance",
+      "name": "Old Grand Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 1.6257893407654658,
+        "pitch": 0.000006367201081047824,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9868800458073466,
+          "pitch": 0.2576767245027298,
+          "rotation": 0,
+          "target": "8-second-floor"
+        },
+        {
+          "yaw": 1.3944217428873689,
+          "pitch": 0.376536460549989,
+          "rotation": 0,
+          "target": "5-ground-floor-room"
+        },
+        {
+          "yaw": -2.0581780226338537,
+          "pitch": 0.49430069952106237,
+          "rotation": 0,
+          "target": "3-receiving-area"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6402248860590962,
+          "pitch": 0.09480843722784726,
+          "title": "Antigua Grand Entrada&nbsp;<div>(Old Grand Entrance)</div>",
+          "text": "The grand staircase with wooden balusters is the main feature that greets every visitor."
+        }
+      ]
+    },
+    {
+      "id": "5-ground-floor-room",
+      "name": "Ground Floor Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -0.01424758573057261,
+        "pitch": 0.0300450116755826,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6725916198476938,
+          "pitch": 0.3479121724361356,
+          "rotation": 0,
+          "target": "6-pasillo-hallway"
+        },
+        {
+          "yaw": -1.729025314834967,
+          "pitch": 0.2278158536388073,
+          "rotation": 0,
+          "target": "4-old-grand-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.40247238989868706,
+          "pitch": -0.021221622460265266,
+          "title": "Habitacion (Room)",
+          "text": "The room for storing old traditional costumes that can be worn for a fee."
+        }
+      ]
+    },
+    {
+      "id": "6-pasillo-hallway",
+      "name": "Pasillo (Hallway)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 1.560955738577011,
+        "pitch": -0.045846200131171244,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5260798671729567,
+          "pitch": 0.25396808415602834,
+          "rotation": 0,
+          "target": "7-pasillo-hallway"
+        },
+        {
+          "yaw": -1.4728757552511684,
+          "pitch": 0.2644557591418071,
+          "rotation": 0,
+          "target": "5-ground-floor-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-pasillo-hallway",
+      "name": "Pasillo (Hallway)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 1.5443213797842024,
+        "pitch": -0.14951665573750716,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4424191759486824,
+          "pitch": 0.301935958400275,
+          "rotation": 0,
+          "target": "6-pasillo-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-second-floor",
+      "name": "Second Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.010685689297929457,
+        "pitch": -0.035049513599245685,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16827293698147372,
+          "pitch": 0.24488889510215017,
+          "rotation": 0,
+          "target": "9-main-living-room"
+        },
+        {
+          "yaw": -2.940647360921334,
+          "pitch": 0.7710373159460637,
+          "rotation": 0,
+          "target": "4-old-grand-entrance"
+        },
+        {
+          "yaw": -2.9316559140696885,
+          "pitch": 0.3171835594706387,
+          "rotation": 0,
+          "target": "10-pasillo-hallway"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.025392041824249,
+          "pitch": 0.08143098471410148,
+          "title": "Habitacion (Hallway)",
+          "text": "The bedroom of the house, where traditional costumes can be worn for a fee."
+        },
+        {
+          "yaw": -2.2962741965617877,
+          "pitch": 0.05288943653203404,
+          "title": "Habitacion (Room)",
+          "text": "The second bedroom of the house."
+        }
+      ]
+    },
+    {
+      "id": "9-main-living-room",
+      "name": "Main Living Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.9279238779188717,
+        "pitch": -0.13269513309796466,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0018849154673646,
+          "pitch": 0.26659435409818677,
+          "rotation": 0,
+          "target": "8-second-floor"
+        },
+        {
+          "yaw": -1.689550667835345,
+          "pitch": 0.23475806987781667,
+          "rotation": 0,
+          "target": "10-pasillo-hallway"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.584310601807795,
+          "pitch": 0.08244925665790959,
+          "title": "Sala Principal&nbsp;<div>(Main Living Room)</div>",
+          "text": "The main living room features a high ceiling and is accented by intricately designed lattices. To complement the grand design of the house, original furnitures and fixtures were imported from Europe and US."
+        }
+      ]
+    },
+    {
+      "id": "10-pasillo-hallway",
+      "name": "Pasillo (Hallway)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -1.5850439125254603,
+        "pitch": -0.14817178344735638,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.711166283746504,
+          "pitch": 0.24960263382334347,
+          "rotation": 0,
+          "target": "11-dining-room"
+        },
+        {
+          "yaw": 1.799254096281067,
+          "pitch": 0.2796725331903165,
+          "rotation": 0,
+          "target": "8-second-floor"
+        },
+        {
+          "yaw": 1.393787489758271,
+          "pitch": 0.282417017560892,
+          "rotation": 0,
+          "target": "9-main-living-room"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.36371824866909463,
+          "pitch": 0.07115297230444284,
+          "title": "Habitacion (Room)",
+          "text": "The third bedroom of the house. Large windows with louvered panels are installed to minimize the entry of sunlight during high noon."
+        }
+      ]
+    },
+    {
+      "id": "11-dining-room",
+      "name": "Dining Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.6197687391092241,
+        "pitch": 0.05756947621606834,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4357787099343495,
+          "pitch": 0.32723598719236335,
+          "rotation": 0,
+          "target": "10-pasillo-hallway"
+        },
+        {
+          "yaw": -3.0953466772078873,
+          "pitch": 0.4611469860489219,
+          "rotation": 0,
+          "target": "12-kitchen"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.19482804245009788,
+          "pitch": 0.0900493525034669,
+          "title": "Comedor (Dining Room)",
+          "text": "The area where the family gathers for meals, the room includes a large dining table and traditional Filipino art."
+        }
+      ]
+    },
+    {
+      "id": "12-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -1.560113432259147,
+        "pitch": 0.10262384890692822,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.790965186367357,
+          "pitch": 0.40145948545325894,
+          "rotation": 0,
+          "target": "11-dining-room"
+        },
+        {
+          "yaw": 2.119598490555207,
+          "pitch": 0.5281912197965717,
+          "rotation": 0,
+          "target": "1-grand-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.0865533087566916,
+          "pitch": 0.14246667785329414,
+          "title": "Cocina (Kitchen)",
+          "text": "The area for cooking and food preparation, accompanied by a staircase to the grand entrance for easy access by servants."
+        }
+      ]
+    }
+  ],
+  "name": "Villa Sariaya Ancestral Home",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
